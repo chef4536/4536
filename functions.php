556d9c79bef2553efe49053450c05202d72b7aa4
chef <info@4536.jp>
2019-01-16 11:37:21 +0900

    update

--- functions.php
+++ functions.php
@@ -15,7 +15,6 @@ require_once('js/_init.php');
 //テーマアップデート
 require_once('plugin-update-checker/plugin-update-checker.php');
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-//    'https://4536.jp/wp-content/themes/4536/theme-update.json',
 	'https://raw.githubusercontent.com/chef4536/4536/master/theme-update.json',
 	__FILE__,
 	'4536'
