03128c38f58d91a6c7e64bb274bad22b21324239
chef <info@4536.jp>
2019-03-08 22:50:02 +0900

    update htaccess

--- htaccess-text/redirect-post-in-category.php
+++ htaccess-text/redirect-post-in-category.php
@@ -3,6 +3,9 @@
 $redirect_settings = redirect_post_in_category_settings();
 $rewrite_array = [];
 
+preg_match( '/([^\/]+?)?$/', site_url(), $sub_dir);
+$sub_dir = ( !empty($sub_dir[1]) ) ? '\/'.$sub_dir[1] : '';
+
 for ($i=0; $i < redirect_count(); $i++) {
 	$rewrite_cond = [];
 	$redirect_type = ( $redirect_settings['redirect_check_302'][$i] === '1' ) ? '302' : '301' ;
@@ -21,14 +24,14 @@ for ($i=0; $i < redirect_count(); $i++) {
 	$posts_array = get_posts( $args );
 	foreach ( $posts_array as $post_info ) {
 	  $slug = $post_info->post_name;
-	  $rewrite_cond[] = 'RewriteCond %{REQUEST_URI} .\/'.preg_quote($slug).'\/?$ [OR]';
+	  $rewrite_cond[] = 'RewriteCond %{REQUEST_URI} ^'.$sub_dir.'\/'.preg_quote($slug).'\/?$ [OR]';
 	}
 	$rewrite_cond = implode( PHP_EOL, $rewrite_cond );
 	$rewrite_cond = rtrim( $rewrite_cond, ' [OR]' );
 	$rewrite_cond = $rewrite_cond.PHP_EOL.'RewriteCond %{REQUEST_URI} !(\/category\/.)';
 	$rewrite_cond = $rewrite_cond.PHP_EOL.'RewriteCond %{REQUEST_URI} !(\/tag\/.)';
 	$site_url = rtrim( $site_url, '/' ) . '/';
-	$rewrite = $rewrite_cond.PHP_EOL.'RewriteRule ([^\/]+?)\/?$ '.preg_quote($site_url, '/').'\$1 [R='.$redirect_type.',L]';
+	$rewrite = $rewrite_cond.PHP_EOL.'RewriteRule ([^\/]+?)?$ '.preg_quote($site_url, '/').'\$1 [R='.$redirect_type.',L]';
 	$rewrite = '#setting_no_'.$i.'_begin'.PHP_EOL.$rewrite.PHP_EOL.'#setting_no_'.$i.'_end';
 	$rewrite_array[] = $rewrite;
 }
