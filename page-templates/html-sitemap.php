0da9588ec44cbc7aa5eba258149ce0f3b3c3a1b6
chef <info@4536.jp>
2019-05-02 22:28:01 +0900

    create html-sitemap-page

---
+++ page-templates/html-sitemap.php
@@ -0,0 +1,49 @@
+<?php /* Template Name: すべてのカテゴリと記事（HTMLサイトマップ） */
+get_header(); ?>
+<div id="contents-wrapper">
+  <div id="contents-inner">
+    <main id="main" class="padding-wrap-main-4536 post-bg-color post-color" role="main">
+      <h1 id="h1"><?php the_title(); ?></h1>
+      <div id="html-sitemap" class="post article-body">
+        <?php
+      	$categories = get_categories( 'parent=0' );
+      	foreach( $categories as $category ) {
+          $cat_id = $category->cat_ID;
+      		echo '<li>';
+      		echo '<a href="' . get_category_link( $cat_id ) . '">' . $category->name . '</a>';
+          get_child_cat_4536( $cat_id );
+      		echo '</li>';
+      	}
+        function get_child_cat_4536( $cat_id ) {
+          $child_cat_id_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
+          if( !empty( $child_cat_id_arr ) ) {
+            echo '<ul class="children">';
+            foreach( $child_cat_id_arr as $child_cat ) {
+              $child_cat_id = $child_cat->term_id;
+              echo '<li>';
+              echo '<a href="' . get_category_link( $child_cat_id ) . '">' . $child_cat->name . '</a>';
+              // $post_arr = get_posts( 'post_type=post&posts_per_page=-1&cat=' . $child_cat_id );
+              // echo '<ul class="children">';
+              // $post_id = [];
+              // foreach( $post_arr as $post ) {
+              //   $post_id[] = $post->ID;
+              //   echo '<li>' . $post->post_title . '</li>';
+              // }
+              // echo '</ul>';
+              get_child_cat_4536( $child_cat_id );
+              echo '</li>';
+            }
+            echo '</ul>';
+          }
+        }
+        ?>
+      </div>
+    </main>
+    <?php
+    get_template_part( 'template-parts/music' );
+    get_template_part( 'template-parts/movie' );
+    get_template_part( 'template-parts/pickup' );
+    ?>
+  </div>
+</div>
+<?php get_sidebar().get_footer();
