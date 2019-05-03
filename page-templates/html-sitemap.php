0175ecce971eaefac2c2222de0b41940cb845846
chef <info@4536.jp>
2019-05-03 23:39:24 +0900

    update html-sitemap

--- page-templates/html-sitemap.php
+++ page-templates/html-sitemap.php
@@ -3,57 +3,63 @@ get_header(); ?>
 <div id="contents-wrapper">
   <div id="contents-inner">
     <main id="main" class="padding-wrap-main-4536 post-bg-color post-color" role="main">
-      <h1 id="h1"><?php the_title(); ?></h1>
-      <div id="html-sitemap" class="post article-body">
-        <?php
-      	$categories = get_categories( 'parent=0' );
-      	foreach( $categories as $category ) {
-          echo '<section>';
-          $cat_id = $category->cat_ID;
-      		echo '<a href="' . get_category_link( $cat_id ) . '"><h2>' . $category->name . '</h2></a>';
-          $thumbnail_arr = get_posts( 'post_type=post&posts_per_page=1&category=' . $cat_id );
-          if( has_post_thumbnail( $post_id = $thumbnail_arr[0]->ID ) ) {
-            echo '<figure id="post-thumbnail-4536" class="alignwide">' . get_the_post_thumbnail( $post_id ) . '</figure>';
-          }
-          $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
-          $exclude_cat_id = '';
-          foreach( $child_cat_arr as $child_cat ) {
-            $exclude_cat_id .= ',-' . $child_cat->term_id;
-          }
-          $post_arr = get_posts( 'post_type=post&posts_per_page=-1&category=' . $cat_id . $exclude_cat_id );
-          foreach( $post_arr as $post ) {
-            echo '<article class="post-list"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="far fa-file-alt"></i>' . $post->post_title . '</a></article>';
-          }
-          the_child_sitemap_4536( $cat_id, 2 );
-          echo '</section>';
-      	}
-
-        function the_child_sitemap_4536( $cat_id, $i ) {
-          $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
-          if( !empty( $child_cat_arr ) ) {
-            $i++;
-            if( $i > 6 ) $i = 6;
+      <article id="html-sitemap" class="post">
+        <header>
+          <h1 id="h1"><?php the_title(); ?></h1>
+          <?php the_post_thumbnail_4536(); ?>
+        </header>
+        <div class="article-body">
+          <?php
+          echo apply_filters( 'the_content', $post->post_content );
+        	$categories = get_categories( 'parent=0' );
+        	foreach( $categories as $category ) {
+            echo '<section>';
+            $cat_id = $category->cat_ID;
+        		echo '<a href="' . get_category_link( $cat_id ) . '"><h2>' . $category->name . '</h2></a>';
+            $thumbnail_arr = get_posts( 'post_type=post&posts_per_page=1&category=' . $cat_id );
+            if( has_post_thumbnail( $post_id = $thumbnail_arr[0]->ID ) ) {
+              echo '<figure class="alignwide margin-bottom-1_5em">' . get_the_post_thumbnail( $post_id ) . '</figure>';
+            }
+            $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
+            $exclude_cat_id = '';
             foreach( $child_cat_arr as $child_cat ) {
-              echo '<section class="children">';
-              $child_cat_id = $child_cat->term_id;
-              echo '<a href="' . get_category_link( $child_cat_id ) . '"><h' . $i . '>' . $child_cat->name . '</h' . $i . '></a>';
-              $exclude_cat_id = '';
-              $exclude_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$child_cat_id ]);
-              foreach( $exclude_cat_arr as $obj ) {
-                $exclude_cat_id .= ',-' . $obj->term_id;
+              $exclude_cat_id .= ',-' . $child_cat->term_id;
+            }
+            $post_arr = get_posts( 'post_type=post&posts_per_page=-1&category=' . $cat_id . $exclude_cat_id );
+            foreach( $post_arr as $post ) {
+              echo '<article class="post-list line-height-1_4"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="far fa-file-alt"></i>' . $post->post_title . '</a></article>';
+            }
+            the_child_sitemap_4536( $cat_id, 2 );
+            echo '</section>';
+        	}
+
+          function the_child_sitemap_4536( $cat_id, $i ) {
+            $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
+            if( !empty( $child_cat_arr ) ) {
+              $i++;
+              if( $i > 6 ) $i = 6;
+              foreach( $child_cat_arr as $child_cat ) {
+                echo '<section class="children">';
+                $child_cat_id = $child_cat->term_id;
+                echo '<a href="' . get_category_link( $child_cat_id ) . '"><h' . $i . '>' . $child_cat->name . '</h' . $i . '></a>';
+                $exclude_cat_id = '';
+                $exclude_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$child_cat_id ]);
+                foreach( $exclude_cat_arr as $obj ) {
+                  $exclude_cat_id .= ',-' . $obj->term_id;
+                }
+                $post_arr = get_posts( 'post_type=post&posts_per_page=-1&cat=' . $child_cat_id . $exclude_cat_id );
+                foreach( $post_arr as $post ) {
+                  echo '<article class="post-list line-height-1_4"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="far fa-file-alt"></i>' . $post->post_title . '</a></article>';
+                }
+                the_child_sitemap_4536( $child_cat_id, $i );
+                echo '</section>';
               }
-              $post_arr = get_posts( 'post_type=post&posts_per_page=-1&cat=' . $child_cat_id . $exclude_cat_id );
-              foreach( $post_arr as $post ) {
-                echo '<article class="post-list"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="far fa-file-alt"></i>' . $post->post_title . '</a></article>';
-              }
-              the_child_sitemap_4536( $child_cat_id, $i );
-              echo '</section>';
             }
           }
-        }
 
-        ?>
-      </div>
+          ?>
+        </div>
+      </article>
     </main>
     <?php
     get_template_part( 'template-parts/music' );
