8e89a5623e67abe006091d1732f1c20ff1de8c23
chef <info@4536.jp>
2019-05-03 22:38:59 +0900

    update

--- page-templates/html-sitemap.php
+++ page-templates/html-sitemap.php
@@ -11,54 +11,45 @@ get_header(); ?>
           echo '<section>';
           $cat_id = $category->cat_ID;
       		echo '<a href="' . get_category_link( $cat_id ) . '"><h2>' . $category->name . '</h2></a>';
-          $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
-          foreach( $child_cat_arr as $child_cat ) {
-            $child_cat_id .= ',-' . $child_cat->term_id;
-          }
-          $post_arr = get_posts( 'post_type=post&posts_per_page=-1&category=' . $cat_id . $child_cat_id );
-          if( has_post_thumbnail( $post_id = $post_arr[0]->ID ) ) {
+          $thumbnail_arr = get_posts( 'post_type=post&posts_per_page=1&category=' . $cat_id );
+          if( has_post_thumbnail( $post_id = $thumbnail_arr[0]->ID ) ) {
             echo '<figure id="post-thumbnail-4536" class="alignwide">' . get_the_post_thumbnail( $post_id ) . '</figure>';
           }
+          $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
+          $exclude_cat_id = '';
+          foreach( $child_cat_arr as $child_cat ) {
+            $exclude_cat_id .= ',-' . $child_cat->term_id;
+          }
+          $post_arr = get_posts( 'post_type=post&posts_per_page=-1&category=' . $cat_id . $exclude_cat_id );
           foreach( $post_arr as $post ) {
-            echo '<article class="post-list"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="fas fa-angle-right"></i>' . $post->post_title . '</a></article>';
+            echo '<article class="post-list"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="far fa-file-alt"></i>' . $post->post_title . '</a></article>';
           }
           the_child_sitemap_4536( $cat_id, 2 );
           echo '</section>';
       	}
 
         function the_child_sitemap_4536( $cat_id, $i ) {
-          $child_cat_id_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
-          if( !empty( $child_cat_id_arr ) ) {
-            // if( empty( get_ancestors( $cat_id, 'category' ) ) ) echo '<section class="children">';
+          $child_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$cat_id ]);
+          if( !empty( $child_cat_arr ) ) {
             $i++;
-            foreach( $child_cat_id_arr as $child_cat ) {
+            if( $i > 6 ) $i = 6;
+            foreach( $child_cat_arr as $child_cat ) {
               echo '<section class="children">';
               $child_cat_id = $child_cat->term_id;
               echo '<a href="' . get_category_link( $child_cat_id ) . '"><h' . $i . '>' . $child_cat->name . '</h' . $i . '></a>';
               $exclude_cat_id = '';
-              $exclude_cat_id_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$child_cat_id ]);
-              foreach( $exclude_cat_id_arr as $obj ) {
+              $exclude_cat_arr = get_terms([ 'taxonomy'=>'category', 'parent'=>$child_cat_id ]);
+              foreach( $exclude_cat_arr as $obj ) {
                 $exclude_cat_id .= ',-' . $obj->term_id;
               }
-              $exclude_post_id = [];
               $post_arr = get_posts( 'post_type=post&posts_per_page=-1&cat=' . $child_cat_id . $exclude_cat_id );
               foreach( $post_arr as $post ) {
-                $exclude_post_id[] = $post->ID;
-                echo '<article class="post-list"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="fas fa-angle-right"></i>' . $post->post_title . '</a></article>';
+                echo '<article class="post-list"><a class="display-block padding-bottom-1em" href="' . get_the_permalink( $post->ID ) . '"><i class="far fa-file-alt"></i>' . $post->post_title . '</a></article>';
               }
               the_child_sitemap_4536( $child_cat_id, $i );
               echo '</section>';
             }
-            // if( empty( get_ancestors( $cat_id, 'category' ) ) ) echo '</section>';
           }
-
-          // if( empty( get_ancestors( $cat_id, 'category' ) ) ) {
-          //   $post_arr = get_posts( 'post_type=post&posts_per_page=-1&cat=' . $cat_id );
-          //   foreach( $post_arr as $post ) {
-          //     echo '<article><a href="' . get_the_permalink( $post->ID ) . '"><i class="fas fa-angle-right"></i>' . $post->post_title . '</a></article>';
-          //   }
-          // }
-
         }
 
         ?>
