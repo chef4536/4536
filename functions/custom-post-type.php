283b08d82d168f112577e5687fc96fd3f13f4337
chef <info@4536.jp>
2019-05-07 10:56:32 +0900

    fixed custom post type

--- functions/custom-post-type.php
+++ functions/custom-post-type.php
@@ -1,39 +1,57 @@
 <?php
 add_action( 'init', function() {
-  $main_post_type = esc_html(get_option('main_media_slug'));
-  $admin_main_media = esc_html(get_option('admin_main_media'));
-  $sub_post_type = esc_html(get_option('sub_media_slug'));
-  $admin_sub_media = esc_html(get_option('admin_sub_media'));
+  $main_post_type = esc_html( get_option('main_media_slug') );
+  $admin_main_media = esc_html( get_option('admin_main_media') );
+  $sub_post_type = esc_html( get_option('sub_media_slug') );
+  $admin_sub_media = esc_html( get_option('admin_sub_media') );
   // ミュージック
-  if($admin_main_media) {
-  register_post_type( 'music' , [
+  if( !empty( $admin_main_media ) ) {
+    register_post_type( 'music' , [
       'labels' => [
-          'name' => __( $admin_main_media ),
-          'singular_name' => __( $admin_main_media ),
-          'all_items' => __( $admin_main_media.' 一覧' )
+        'name' => __( $admin_main_media ),
+        'singular_name' => __( $admin_main_media ),
+        'all_items' => __( $admin_main_media.' 一覧' )
       ],
       'public' => true,
       'show_in_rest' => true,
       'has_archive' => true,
-      'rewrite' => ['slug' => $main_post_type],
+      'rewrite' => [
+        'slug' => $main_post_type
+      ],
       'menu_position' => 5,
-      'supports' => ['title','editor','thumbnail','revisions','comments'],
-  ]);
+      'supports' => [
+        'title',
+        'editor',
+        'thumbnail',
+        'revisions',
+        'comments',
+        'author',
+      ],
+    ]);
   }
   // ムービー
   if($admin_sub_media) {
-  register_post_type( 'movie',[
+    register_post_type( 'movie', [
       'labels' => [
-          'name' => __( $admin_sub_media ),
-          'singular_name' => __( $admin_sub_media ),
-          'all_items' => __( $admin_sub_media.' 一覧' )
+        'name' => __( $admin_sub_media ),
+        'singular_name' => __( $admin_sub_media ),
+        'all_items' => __( $admin_sub_media.' 一覧' )
       ],
       'public' => true,
       'show_in_rest' => true,
       'has_archive' => true,
-      'rewrite' => ['slug' => $sub_post_type],
+      'rewrite' => [
+        'slug' => $sub_post_type
+      ],
       'menu_position' => 5,
-      'supports' => ['title','editor','thumbnail','revisions','comments'],
-  ]);
+      'supports' => [
+        'title',
+        'editor',
+        'thumbnail',
+        'revisions',
+        'comments',
+        'author',
+      ],
+    ]);
   }
 });
