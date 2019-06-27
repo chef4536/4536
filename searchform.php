e6ab735d73cddb2cd7fb23f4e108195db5e728ce
chef <info@4536.jp>
2019-06-27 23:48:04 +0900

    update

--- searchform.php
+++ searchform.php
@@ -7,9 +7,9 @@ if(search_style()==="google_custom_search") {
     $query = 'q';
 }
 if( is_amp() && is_ssl() ) $target = 'target="_top"';
-$search_form = '<form id="searchform" class="d-f" role="search" method="get" action="'.home_url().'/'.$slug.'" '.$target.'>'.
-    '<input type="search" value="'.get_search_query().'" name="'.$query.'" id="'.$query.'" placeholder="キーワード" />'.
-    '<button type="submit" id="searchsubmit"><i class="fas fa-search" aria-hidden="true"></i></button>'.
+$search_form = '<form id="searchform" data-display="flex" data-align-items="center" class="flex" role="search" method="get" action="'.home_url().'/'.$slug.'" '.$target.'>'.
+    '<input type="search" value="'.get_search_query().'" name="'.$query.'" id="'.$query.'" placeholder="キーワード" class="flex pt-2 pb-2" />'.
+    '<button type="submit" id="searchsubmit" class="pa-2 l-h-100">' . icon_4536('search', '', 24) .'</button>'.
     '</form>';
 if( is_amp() && !is_ssl() ) $search_form = null;
 echo $search_form;
