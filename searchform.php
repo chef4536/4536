074618c5843d0a3d6af3c9e9d36a3291abaa0e94
chef <info@4536.jp>
2019-06-28 12:46:30 +0900

    update

--- searchform.php
+++ searchform.php
@@ -1,15 +1,21 @@
 <?php
-$slug = null;
+$slug = '';
 $query = 's';
-$target = null;
-if(search_style()==="google_custom_search") {
+$target = '';
+if (search_style()==="google_custom_search") {
     $slug = google_custom_search_slug();
     $query = 'q';
 }
-if( is_amp() && is_ssl() ) $target = 'target="_top"';
-$search_form = '<form id="searchform" data-display="flex" data-align-items="center" class="flex" role="search" method="get" action="'.home_url().'/'.$slug.'" '.$target.'>'.
-    '<input type="search" value="'.get_search_query().'" name="'.$query.'" id="'.$query.'" placeholder="キーワード" class="flex pt-2 pb-2" />'.
-    '<button type="submit" id="searchsubmit" class="pa-2 l-h-100">' . icon_4536('search', '', 24) .'</button>'.
-    '</form>';
-if( is_amp() && !is_ssl() ) $search_form = null;
-echo $search_form;
+if (is_amp() && is_ssl()) {
+    $target = ' target="_top"';
+}
+if (is_amp() && !is_ssl()) {
+    return;
+} else { ?>
+  <form id="searchform" data-display="flex" data-align-items="center" class="flex" role="search" method="get" action="<?php echo home_url() . '/' . $slug; ?>"<?php echo $target; ?>>
+    <input type="search" value="<?php echo get_search_query(); ?>" name="<?php echo $query; ?>" id="<?php echo $query; ?>" placeholder="キーワード" class="flex pa-2" />
+    <button type="submit" id="searchsubmit" class="pa-1 ml-1 l-h-100">
+      <?php echo icon_4536('search', '', 24); ?>
+    </button>
+  </form>
+<?php }
