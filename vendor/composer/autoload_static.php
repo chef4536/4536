fa60b64e45ee9b9509569d3d30b19f8d2a1a7e16
Shinobi Works <support@shinobiworks.com>
2020-06-19 11:02:59 +0900

    settings

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -39,8 +39,8 @@ class ComposerStaticInit830ef18d066877b1f71969cc126ac300
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
         ),
         'Webmozart\\Assert\\' => 
         array (
